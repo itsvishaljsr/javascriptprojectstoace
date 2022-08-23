47257607af9e5f0bfeff507511af7489a93eb059
Vishal Prajapati <vishalprajapatijsr@gmail.com>
2022-08-23 06:52:18 +0530

    global variables declared
    global variables declared

---
+++ form-validation-in-javascript/script.js
@@ -0,0 +1,6 @@
+// global constant variables declared here
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password_validate = document.getElementById('password_validate');
